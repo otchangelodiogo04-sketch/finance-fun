968d4f1ae307cafe273d1ebadd0d3f42cd288452
Otchangelo Diogo <otchangelodiogo04@gmail.com>
2026-02-15 01:02:08 +0530

    epá

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import Profile from "./pages/Profile";
 import CashFlow from "./pages/CashFlow";
 import Stock from "./pages/Stock";
 import AdminDashboard from "./pages/AdminDashboard";
-import FinanceAi from "./Bot/index.html";
+import FinanceAi from "../Bot/pages/index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -105,7 +105,7 @@ const AppContent = () => {
           <Route path="fluxo-caixa" element={<CashFlow />} />
           <Route path="stock" element={<Stock />} />
           <Route path="admin" element={<AdminDashboard />} />
-          <Route path=".Bot/index.html" element={<FinanceAi />} />
+          <Route path="..Bot/pages/index" element={<FinanceAi />} />
         </Route>
 
         {/* Catch all */}
